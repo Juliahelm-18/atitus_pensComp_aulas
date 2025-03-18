d157994aea576c9b0b39593efbed4c1947fd99ad
Juliahelm-18 <juliahelmrf@gmail.com>
2025-03-18 19:55:04 -0300

    Update converte_horas.py

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -1,18 +1,15 @@
 def hora_para_minuto(valor):
-    pass
     return valor * 60
 
 def minuto_para_segundo(valor):
-    pass
     return valor * 60
 
 def hora_para_segundo(valor):
-    pass
-    return valor * 60 * 60
+    return valor * 60**2
 
 def dia_para_segundo(valor):
-    pass
-    return valor * 24 * 60 * 60
+    return hora_para_segundo(valor * 24)
+
 
 assert hora_para_minuto(0) == 0
 assert hora_para_minuto(1) == 60
@@ -21,13 +18,13 @@ assert hora_para_minuto(2) == 120
 assert minuto_para_segundo(0) == 0
 assert minuto_para_segundo(1) == 60
 assert minuto_para_segundo(2) == 120
-
+ 
 assert hora_para_segundo(0) == 0
 assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
 assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
 
-assert dia_para_segundo(0) == 0
-assert dia_para_segundo(1) == 86400
-assert dia_para_segundo(2) == 172800
+assert dia_para_segundo(0)==0
+assert dia_para_segundo(1)==86.400
+assert dia_para_segundo(2)==172.800
 
 print("Terminou com sucesso!")
