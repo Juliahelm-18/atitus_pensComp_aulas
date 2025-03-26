4c4aaac99ec9bf54888d9d42fed1d21091ff3f69
Juliahelm-18 <juliahelmrf@gmail.com>
2025-03-26 13:27:14 -0300

    Update bissexto.py

--- 04_operadores_logicos_e_condicoes/bissexto.py
+++ 04_operadores_logicos_e_condicoes/bissexto.py
@@ -10,12 +10,16 @@ def verificar_bissexto(ano):
   ano = input("Digite um ano")
    if ano % 4 == 0:
       return True
+      print("O ano é bissexto")
    if ano % 4 == 3:
-      return (ano + 1) True 
+      return (ano + 1) True
+      print("O ano (ano + 1) será bissexto")
    if ano % 4 == 2:
        return (ano + 2) True
+      print("O ano (ano + 2) será bissexto")
    if ano % 4 == 1:
       return (ano + 3) True
+      print("O ano (ano + 3) será bissexto")
    
 
 def test():
