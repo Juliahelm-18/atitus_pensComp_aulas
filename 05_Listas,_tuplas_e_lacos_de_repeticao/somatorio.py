09163f9924c8ec09857385d0622a3cc16ce4abf8
Juliahelm-18 <juliahelmrf@gmail.com>
2025-03-31 12:19:12 -0300

    alterações

---
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/somatorio.py
@@ -0,0 +1,26 @@
+def somatorio(numero):
+    if numero < 0:
+        return None
+    
+    if numero = 0:
+        return numero = 1
+    if numero = 1:
+        return numero = 1
+
+    soma = 0
+
+    while soma in range(10):
+        return soma
+
+    def test():    
+    assert somatorio(-1) is None
+    assert somatorio(0) == 0
+    assert somatorio(1) == 1
+    assert somatorio(2) == 3
+    assert somatorio(3) == 6
+    assert somatorio(4) == 10
+    assert somatorio(5) == 15
+    assert somatorio(6) == 21
+    assert somatorio(7) == 28
+    assert somatorio(8) == 36
+    assert somatorio(9) == 45
