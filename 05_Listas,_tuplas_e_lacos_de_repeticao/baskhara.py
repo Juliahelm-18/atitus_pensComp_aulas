1633b2cf1a068465eb477df80d972f6b8de8b5ff
Juliahelm-18 <juliahelmrf@gmail.com>
2025-03-31 12:41:35 -0300

    Update baskhara.py

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -1,10 +1,19 @@
 def baskhara(a, b, c):
-    # retorne None se discriminante < 0
-    # retorne apenas um valor se discriminante == 0
-    # retorne [x1, x2] nos outros casos
-    pass
+    discriminante = b**2 - 4*a*c
 
+while True:
+    if discriminante < 0:
+        return False
+    else:
+        return True
 
+    if discrimante = 0:
+        return "Não é possível realizar a equação"
+
+    x1 = (-b + ((b**2 - 4*a*c) * 1/2)) / (2 * a)
+    x2 = (-b - ((b**2 - 4*a*c) * 1/2)) / (2 * a)
+
+def test ():
 assert baskhara(1, -3, 2) == [2, 1]
 assert baskhara(2, 3, -2) == [-2, 0.5]
 assert baskhara(1, -5, 6) == [2, 3]
