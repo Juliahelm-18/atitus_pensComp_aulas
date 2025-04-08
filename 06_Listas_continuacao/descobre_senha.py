d14bdd9b4840b6a03b9b33b862f9e84cab924839
Juliahelm-18 <juliahelmrf@gmail.com>
2025-04-07 21:54:48 -0300

    Update descobre_senha.py

--- 06_Listas_continuacao/descobre_senha.py
+++ 06_Listas_continuacao/descobre_senha.py
@@ -1,10 +1,10 @@
 def senha(palpite):
-  palpite = int(input("Digite um número entre 0 e 10"))
+  palpite = int(input("Digite um número entre 0 e 10 "))
   senha = 4
   tentativas = 0
   while senha != palpite:
     print("Senha errada")
-    palpite = int(input("Digite um número entre 0 e 10"))
+    palpite = int(input("Digite um número entre 0 e 10 "))
     tentativas += 1
   print("Você acertou a senha!")
   print(f"Essas foram suas tentativas {tentativas}")
