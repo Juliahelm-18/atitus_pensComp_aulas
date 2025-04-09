4453b7a005e25818f8d69b298abfb67a24fdef72
Juliahelm-18 <juliahelmrf@gmail.com>
2025-04-09 19:21:22 -0300

    Update fatorial.py

--- 05_Listas,_tuplas_e_lacos_de_repeticao/fatorial.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/fatorial.py
@@ -5,10 +5,10 @@ for i in range(1, n + 1):
   return *=i 
 
 def test ():
-assert fatorial(0) == 1
-assert fatorial(1) == 1
-assert fatorial(2) == 2
-assert fatorial(3) == 6
-assert fatorial(4) == 24
-assert fatorial(5) == 120
-assert fatorial(-1) is None
+  assert fatorial(0) == 1
+  assert fatorial(1) == 1
+  assert fatorial(2) == 2
+  assert fatorial(3) == 6
+  assert fatorial(4) == 24
+  assert fatorial(5) == 120
+  assert fatorial(-1) is None
