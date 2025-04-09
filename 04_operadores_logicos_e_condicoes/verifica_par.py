ccb39cb4767af37a508517a32d48eaa1927d075d
Juliahelm-18 <juliahelmrf@gmail.com>
2025-04-09 19:20:44 -0300

    Update verifica_par.py

--- 04_operadores_logicos_e_condicoes/verifica_par.py
+++ 04_operadores_logicos_e_condicoes/verifica_par.py
@@ -9,14 +9,14 @@ def eh_impar(numero):
       return not eh_par(numero):
 
 def test ():
-assert eh_par(0)
-assert eh_par(2)
-assert eh_par(4)
-assert not eh_par(1)
-assert not eh_par(3)
+  assert eh_par(0)
+  assert eh_par(2)
+  assert eh_par(4)
+  assert not eh_par(1)
+  assert not eh_par(3)
 
-assert eh_impar(1)
-assert eh_impar(3)
-assert eh_impar(5)
-assert not eh_impar(0)
-assert not eh_impar(2)
+  assert eh_impar(1)
+  assert eh_impar(3)
+  assert eh_impar(5)
+  assert not eh_impar(0)
+  assert not eh_impar(2)
