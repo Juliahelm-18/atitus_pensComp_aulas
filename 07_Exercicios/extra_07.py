801527e5ffe26461a5c81f81089b7e6144a87eae
Juliahelm-18 <juliahelmrf@gmail.com>
2025-04-13 20:49:20 -0300

    Update extra_07.py

--- 07_Exercicios/extra_07.py
+++ 07_Exercicios/extra_07.py
@@ -1,15 +1,19 @@
 def fahrenheit_para_celsius(valor):
-    pass
+  temp = float(input("Diga a temperatura em Fahrenheit: "))
+  f_para_c = (temp - 32) / 1.8
+  print(f"A temperatura em graus Celcius é:  {f_para_c}C°)
 
 
-def celsius_para_fahrenheit(valor):
-    pass
+def celsius_para_farenheit(valor):
+  temp = float(input("Diga a temperatura em Celsius: "))
+  c_para_f = (1.8 * temp) + 32
+  print(f"A temperatura em graus Fahrenheit é: {c_para_f}F°)
 
+def test():
+  assert fahrenheit_para_celsius(104) == 40
+  assert fahrenheit_para_celsius(-13) == -25
 
-assert fahrenheit_para_celsius(104) == 40
-assert fahrenheit_para_celsius(-13) == -25
+  assert celsius_para_fahrenheit(40) == 104
+  assert celsius_para_fahrenheit(-25) == -13
 
-assert celsius_para_fahrenheit(40) == 104
-assert celsius_para_fahrenheit(-25) == -13
-
-assert celsius_para_fahrenheit(fahrenheit_para_celsius(30)) == 30
+  assert celsius_para_fahrenheit(fahrenheit_para_celsius(30)) == 30
