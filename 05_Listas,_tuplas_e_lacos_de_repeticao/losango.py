ce356e3af95e487356fc0a7f639efefbcd7c9e45
Juliahelm-18 <juliahelmrf@gmail.com>
2025-04-13 20:03:52 -0300

    Update losango.py

--- 05_Listas,_tuplas_e_lacos_de_repeticao/losango.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/losango.py
@@ -1,7 +1,16 @@
 def desenha_losango(altura):
-    # preencher
-    pass
+  altura = int(input("Digite um valor ímpar para a altura do losango: "))
+  if altura % 2 == 0:
+      altura = altura + 1
+      
+  meio = altura // 2
+  for linha in range(altura):
+      if linha <= meio:
+          espacos = meio - linha
+          estrelas = linha * 2 + 1 
+      else: 
+          espacos = linha - meio
+          estrelas = altura - (linha - meio) * 2
+      print(' ' * espacos + '#' * estrelas)
 
 
-altura = int(input("Digite um valor ímpar para a altura do losango: "))
-desenha_losango(altura)
