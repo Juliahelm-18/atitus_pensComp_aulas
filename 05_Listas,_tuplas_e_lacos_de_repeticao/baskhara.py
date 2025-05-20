0f953dbab829f5bf72c9df2d3e8c3af32746f2a2
Juliahelm-18 <juliahelmrf@gmail.com>
2025-05-20 19:28:31 -0300

    Update baskhara.py

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -1,23 +1,21 @@
+import math
 def baskhara(a, b, c):
-  discriminante = b**2 - 4*a*c
-
-  while True:
+    discriminante = b**2 - 4*a*c
     if discriminante < 0:
-        return False
+        return None
+    elif discriminante == 0:
+        x = -b / (2 * a)
+        return x
     else:
-        return x1 or x2
+        x1 = (-b + math.sqrt(discriminante)) / (2 * a)
+        x2 = (-b - math.sqrt(discriminante)) / (2 * a)
+        return [x1, x2]
 
-    if discrimante = 0:
-        return "Não é possível realizar a equação"
+def test():
+    assert baskhara(1, -3, 2) == [2, 1]
+    assert baskhara(2, 3, -2) == [0.5, -2]
+    assert baskhara(1, -5, 6) == [3, 2]
+    assert baskhara(1, -7, 10) == [5, 2]
 
-    x1 = (-b + ((b**2 - 4*a*c) * 1/2)) / (2 * a)
-    x2 = (-b - ((b**2 - 4*a*c) * 1/2)) / (2 * a)
-
-def test ():
-  assert baskhara(1, -3, 2) == [2, 1]
-  assert baskhara(2, 3, -2) == [-2, 0.5]
-  assert baskhara(1, -5, 6) == [2, 3]
-  assert baskhara(1, -7, 10) == [2, 5]
-
-  assert baskhara(1, 2, 3) is None
-  assert baskhara(1, 0, 0) == 0
+    assert baskhara(1, 2, 3) is None
+    assert baskhara(1, 0, 0) == 0
