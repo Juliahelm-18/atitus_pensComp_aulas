e05f5eedc911bb8f10d483cedc39b511502e5bfc
Juliahelm-18 <juliahelmrf@gmail.com>
2025-05-19 23:41:59 -0300

    Update bissexto.py

--- 04_operadores_logicos_e_condicoes/bissexto.py
+++ 04_operadores_logicos_e_condicoes/bissexto.py
@@ -13,18 +13,19 @@ def proximo_bissexto(ano):
         return ano + 3
 
 def test():
-     assert eh_bissexto(0)
-     assert eh_bissexto(2020)
-     assert eh_bissexto(2024)
+  assert eh_bissexto(0)
+  assert eh_bissexto(2020)
+  assert eh_bissexto(2024)
 
-     assert not eh_bissexto(1)
-     assert not eh_bissexto(2022)
-     assert not eh_bissexto(2023)
+     
+  assert not eh_bissexto(1)
+  assert not eh_bissexto(2022)
+  assert not eh_bissexto(2023)
 
-     assert proximo_bissexto(2024) == 2024
-     assert proximo_bissexto(2025) == 2028
-     assert proximo_bissexto(2029) == 2032
-     assert proximo_bissexto(2020) == 2020
+  assert proximo_bissexto(2024) == 2024
+  assert proximo_bissexto(2025) == 2028
+  assert proximo_bissexto(2029) == 2032
+  assert proximo_bissexto(2020) == 2020
 
 print(eh_bissexto(2020))
 print(proximo_bissexto(2032))
