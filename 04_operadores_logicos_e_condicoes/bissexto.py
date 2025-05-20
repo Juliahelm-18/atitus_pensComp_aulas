f7751868c4840a4751edcb64943762e7fbf0af54
Juliahelm-18 <juliahelmrf@gmail.com>
2025-05-19 21:45:03 -0300

    Update bissexto.py

--- 04_operadores_logicos_e_condicoes/bissexto.py
+++ 04_operadores_logicos_e_condicoes/bissexto.py
@@ -1,43 +1,30 @@
 def eh_bissexto(ano):
-   if ano % 4 == 0:
-      return True
-   else:
-      return False
+     return (ano % 4) == 0
+
 
 def proximo_bissexto(ano):
-   if ano % 4 == 0:
-      return ano
-   elif ano % 4 == 3:
-      return ano + 1
-   elif ano % 4 == 2:
-      return ano + 2
-   elif ano % 4 == 1:
-      return ano + 3
-      
-   
+    if ano % 4 == 0:
+       return ano
+    if ano % 4 == 3:
+        return ano + 1
+    if ano % 4 == 2:
+        return ano + 2
+    if ano % 4 == 1:
+        return ano + 3
 
 def test():
-   assert eh_bissexto(0) 
-   assert eh_bissexto(2020) 
-   assert eh_bissexto(2024) 
+     assert eh_bissexto(0)
+     assert eh_bissexto(2020)
+     assert eh_bissexto(2024)
 
-   assert not eh_bissexto(1)
-   assert not eh_bissexto(2022)
-   assert not eh_bissexto(2023)
+     assert not eh_bissexto(1)
+     assert not eh_bissexto(2022)
+     assert not eh_bissexto(2023)
 
-   assert proximo_bissexto(2024) == 2024
-   assert proximo_bissexto(2025) == 2028
-   assert proximo_bissexto(2029) == 2032
-   assert proximo_bissexto(2020) == 2020
+     assert proximo_bissexto(2024) == 2024
+     assert proximo_bissexto(2025) == 2028
+     assert proximo_bissexto(2029) == 2032
+     assert proximo_bissexto(2020) == 2020
 
 print(eh_bissexto(2020))
 print(proximo_bissexto(2032))
-
-# solucao_do_professor
-# def eh_bissexto(ano):
-#     return (ano % 4) == 0
-
-# solucao_do_professor
-# def proximo_bissexto(ano):
-#     return (ano + 3) // 4 * 4
-
