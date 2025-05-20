1cd0304670c0384d5471bc03a1102a3de7e77cc7
Juliahelm-18 <juliahelmrf@gmail.com>
2025-05-19 23:30:17 -0300

    Update saudacao.py

--- 10_Introducao_a_funcoes/saudacao.py
+++ 10_Introducao_a_funcoes/saudacao.py
@@ -1,15 +1,17 @@
-def saudacao(nome: str , sobrenome: str, titulo: str) -> str:
-    if titulo is None:
-        return 'Olá, Sr.' + nome + sobrenome
-    elif:
-        return 'Olá, ' + titulo + nome + sobrenome
+from typing import Optional
+
+def saudacao(nome: str, sobrenome: Optional[str] = None, titulo: str = 'Sr.'): -> str:
+    if nome == '':    
+        return "Olá!"    
+    if sobrenome is None:
+        return f"Olá, {titulo} {nome}"
     else: 
-        return 'Olá, ' + titulo + nome
+        return f"Olá, {titulo} {nome} {sobrenome}"
 
 
 def test():
-  assert saudacao("Matheus") == "Olá, Sr. Matheus"
-  assert saudacao("Matheus", "Jardim") == "Olá, Sr. Matheus Jardim"
-  assert saudacao("Matheus", "Jardim", "Prof") == "Olá, Prof Matheus Jardim"
-  assert saudacao("Matheus", titulo="Prof") == "Olá, Prof Matheus"
-  assert saudacao("") == "Olá!"
+    assert saudacao("Matheus") == "Olá, Sr. Matheus"
+    assert saudacao("Matheus", "Jardim") == "Olá, Sr. Matheus Jardim"
+    assert saudacao("Matheus", "Jardim", "Prof") == "Olá, Prof Matheus Jardim"
+    assert saudacao("Matheus", titulo="Prof") == "Olá, Prof Matheus"
+    assert saudacao("") == "Olá!"
