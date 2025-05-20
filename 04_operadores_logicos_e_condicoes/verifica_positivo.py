56bbe25cd2c1fc40bb22d58e53a232d7ccdf7983
Juliahelm-18 <juliahelmrf@gmail.com>
2025-05-19 21:46:58 -0300

    Update verifica_positivo.py

--- 04_operadores_logicos_e_condicoes/verifica_positivo.py
+++ 04_operadores_logicos_e_condicoes/verifica_positivo.py
@@ -1,23 +1,20 @@
-def eh_positivo(valor):
-   if valor > 0:
-      return True
-   else:
-      return False
+def eh_positivo(numero):
+    return numero > 0 
 
-def eh_negativo(valor):
-      return not eh_positivo(valor):
+def eh_negativo(numero):
+     return not eh_positivo(valor):
+    
+def test():
+    assert eh_positivo(1)
+    assert eh_positivo(2)
+    assert eh_positivo(10)
+    assert not eh_positivo(0)
+    assert not eh_positivo(-1)
+    assert not eh_positivo(-10)
 
-def test ():
-  assert eh_positivo(1)
-  assert eh_positivo(2)
-  assert eh_positivo(10)
-  assert not eh_positivo(0)
-  assert not eh_positivo(-1)
-  assert not eh_positivo(-10)
-
-  assert eh_negativo(-1)
-  assert eh_negativo(-2)
-  assert eh_negativo(-10)
-  assert not eh_negativo(0)
-  assert not eh_negativo(1)
-  assert not eh_negativo(10)
+    assert eh_negativo(-1)
+    assert eh_negativo(-2)
+    assert eh_negativo(-10)
+    assert not eh_negativo(0)
+    assert not eh_negativo(1)
+    assert not eh_negativo(10)
