7dd97a862b24447d9503fd26307c2837c325a7d6
Juliahelm-18 <juliahelmrf@gmail.com>
2025-05-19 21:46:30 -0300

    Update verifica_par.py

--- 04_operadores_logicos_e_condicoes/verifica_par.py
+++ 04_operadores_logicos_e_condicoes/verifica_par.py
@@ -1,22 +1,24 @@
 def eh_par(numero):
-   if numero % 2 == 0:
-       return True
-   else:
-       return False
-
+    if numero % 2 == 0:
+        return True
+    else:
+        return False
 
 def eh_impar(numero):
-      return not eh_par(numero):
+    return not eh_par(numero):
 
-def test ():
-  assert eh_par(0)
-  assert eh_par(2)
-  assert eh_par(4)
-  assert not eh_par(1)
-  assert not eh_par(3)
+def test():
+    assert eh_par(0)
+    assert eh_par(2)
+    assert eh_par(4)
+    assert not eh_par(1)
+    assert not eh_par(3)
 
-  assert eh_impar(1)
-  assert eh_impar(3)
-  assert eh_impar(5)
-  assert not eh_impar(0)
-  assert not eh_impar(2)
+    assert eh_impar(1)
+    assert eh_impar(3)
+    assert eh_impar(5)
+    assert not eh_impar(0)
+    assert not eh_impar(2)
+    
+print(eh_par(4))
+print(eh_impar(6))
