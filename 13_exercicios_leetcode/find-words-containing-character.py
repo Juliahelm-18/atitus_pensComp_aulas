3fa242bfb75e822319cbd4de6e126123f30c9b24
Juliahelm-18 <juliahelmrf@gmail.com>
2025-06-22 20:11:24 -0300

    Update find-words-containing-character.py

--- 13_exercicios_leetcode/find-words-containing-character.py
+++ 13_exercicios_leetcode/find-words-containing-character.py
@@ -2,14 +2,19 @@
 
 
 def findWordsContaining(words, letter):
-    pass
+    resultado = []
+    for i, word in enumerate(words):
+        if letter in word:
+            resultado.append(i) 
+    return resultado
 
 
+def test():
 # Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
-assert findWordsContaining(words=["leet", "code"], letter="e") == [0, 1]
+  assert findWordsContaining(words=["leet", "code"], letter="e") == [0, 1]
 
 # Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
-assert findWordsContaining(words=["abc", "bcd", "aaaa", "cbc"], letter="a") == [0, 2]
+  assert findWordsContaining(words=["abc", "bcd", "aaaa", "cbc"], letter="a") == [0, 2]
 
 # Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
-assert findWordsContaining(words=["abc", "bcd", "aaaa", "cbc"], letter="z") == []
+  assert findWordsContaining(words=["abc", "bcd", "aaaa", "cbc"], letter="z") == []
