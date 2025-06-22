f386576f8fe8ff3ce0d402b280a6d228222444b8
Juliahelm-18 <juliahelmrf@gmail.com>
2025-06-22 20:02:18 -0300

    Update parcelamento.py

--- 14_manipulacao_de_datas/parcelamento.py
+++ 14_manipulacao_de_datas/parcelamento.py
@@ -2,28 +2,40 @@ from datetime import date
 
 
 def parcelamento(valor, parcelas, dt_venda):
-    pass
+    lista_parcelas[]
+    if parcelas == 0 or parcelas > valor:
+        return None 
+    
+    for i in range(vezes)
+        if i == vezes - 1
+            parcela  == (valor// vezes) + (valor % vezes)
+            lista_parcelas.append(parcela)
+            return lista_parcelas
+        else:
+            parcela = valor // vezes
+            lista_parcelas.append(parcela)
 
 
 data_venda = date(2025, 1, 31)
 
-assert parcelamento(100, 1, data_venda) == [[100, data_venda]]
-assert parcelamento(100, 2, data_venda) == [
+def test():
+  assert parcelamento(100, 1, data_venda) == [[100, data_venda]]
+  assert parcelamento(100, 2, data_venda) == [
     [50, data_venda],
     [50, date(2025, 2, 28)]
 ]
-assert parcelamento(100, 3, data_venda) == [
+  assert parcelamento(100, 3, data_venda) == [
     [33, data_venda],
     [33, date(2025, 2, 28)],
     [34, date(2025, 3, 31)]
 ]
-assert parcelamento(100, 3, data_venda) == [
+  assert parcelamento(100, 3, data_venda) == [
     [25, data_venda],
     [25, date(2025, 2, 28)],
     [25, date(2025, 3, 31)],
     [25, date(2025, 4, 30)]
 ]
-assert parcelamento(100, 6, data_venda) == [
+  assert parcelamento(100, 6, data_venda) == [
     [16, data_venda],
     [16, date(2025, 2, 28)],
     [17, date(2025, 3, 31)],
