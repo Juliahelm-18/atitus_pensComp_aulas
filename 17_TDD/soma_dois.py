5e9d6fed34adfc07d20c5939ea9881082b6ff8c1
Juliahelm-18 <juliahelmrf@gmail.com>
2025-06-22 20:05:26 -0300

    Update soma_dois.py

--- 17_TDD/soma_dois.py
+++ 17_TDD/soma_dois.py
@@ -1,10 +1,22 @@
 def soma_dois(lista: list, alvo: int) -> list:
-    pass
+    visto = {}
+    
+    for i, num in enumerate(lista):
+        complemento = alvo - num
+        
+
+        if complemento in visto:
+            return [visto[complemento], i]
+        
+        visto[num] = i
+
+    return None
 
 
-assert soma_dois([2, 7, 11, 15], 9) == [0, 1]  # l[0]+l[1] == 9
-assert soma_dois([3, 2, 4], 6) == [1, 2]
-assert soma_dois([3, 3], 6) == [0, 1]
-assert soma_dois([], 6) is None
-assert soma_dois([1], 6) is None
-assert soma_dois([1, 2], 6) is None
+def test():
+  assert soma_dois([2, 7, 11, 15], 9) == [0, 1]  # l[0]+l[1] == 9
+  assert soma_dois([3, 2, 4], 6) == [1, 2]
+  assert soma_dois([3, 3], 6) == [0, 1]
+  assert soma_dois([], 6) is None
+  assert soma_dois([1], 6) is None
+  assert soma_dois([1, 2], 6) is None
